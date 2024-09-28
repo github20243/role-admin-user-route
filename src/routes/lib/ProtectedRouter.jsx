d7d7358369cd9976fd2de36f40ace9bd386c9e3a
nurs_996 <98372@gamil.com>
2024-09-28 18:20:56 +0600

    ProtectedROuter

--- src/routes/lib/ProtectedRouter.jsx
+++ src/routes/lib/ProtectedRouter.jsx
@@ -3,18 +3,15 @@ import { Navigate, useNavigate } from "react-router-dom";
 
 const PrivateRouter = ({ Component, fallBackPath }) => {
 	const [isAuth, setIsAuth] = useState(() => {
-		// Инициализируем состояние, проверяя текущий статус аутентификации
 		return localStorage.getItem("isAuthenticated") === "true";
 	});
 
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		// Проверяем актуальное состояние аутентификации при монтировании компонента
 		const authStatus = localStorage.getItem("isAuthenticated") === "true";
 		setIsAuth(authStatus);
 
-		// Если не аутентифицирован, перенаправляем на fallBackPath
 		if (!authStatus) {
 			navigate(fallBackPath);
 		}
